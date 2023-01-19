aaf6a143ee858585ea23d1a643dd67124a382c41
Medovik <medovik9696@gmail.com>
2023-01-19 20:08:35 +0100

    Lesson 3 HW

---
+++ src/Lesson_3/Operators.java
@@ -0,0 +1,13 @@
+package Lesson_3;
+
+public class Operators {
+    static void Addition(int a, int b){
+        System.out.println("a+b = "+ (a+b));
+    }
+
+    public static void main (String[] args){
+        Addition(5,6);
+    }
+
+
+}
